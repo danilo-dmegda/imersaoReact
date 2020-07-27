901cc7d57e3320d0c6ce5dcb6cfef4b3064dc779
Danilo Megda Ferreira <danilo.megda@gmail.com>
2020-07-27 19:36:08 -0300

    Adicionando os dados iniciais na pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import Menu  from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain  from './components/BannerMain'
+import Carrousel  from './components/Carousel'
+import Footer  from './components/Footer'
 
 function App() {
   return (
     <div>
       <Menu />
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"bla"} />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+      <Carrousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+      <Footer />
     </div>
   );
 }
